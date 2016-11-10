829c100e901ca0816208074b7a793084f564e932
Harish Naidu <harishnaidu@gmail.com>
2016-11-10 19:03:07 +0530

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'visitors#new'
+  resources :contacts, only: [:new, :create]
+  root to: 'visitors#new'
 end
 
 
