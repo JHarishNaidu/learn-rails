bbcebe8f9392c36f9c8a81fb855b65348fa15bae
Harish Naidu <harishnaidu@gmail.com>
2016-11-11 20:42:26 +0530

    assets compiled for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,22 @@ Rails.application.config do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+# email enabled in production
+config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain: ENV["DOMAIN_NAME"],
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: ENV["GMAIL_USERNAME"],
+password: ENV["GMAIL_PASSWORD"]
+}
+
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
